2f010e38ff680d4f8860924391f06243d1b344b0
QTBoy-web <oleg.fomin2006@gmail.com>
2026-06-10 19:20:15 +0400

    Update main.cpp

--- Dynamic/main.cpp
+++ Dynamic/main.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main() {
     cout << "Hello, Prosyanov Viktor Nikolaevich" << endl;
     cout << "Hello, Chernov Nikita Aleksandrovich" <<endl;
+    cout << "Hello, Fomin Oleg Victorovich" << endl;
     return 0;
 }
